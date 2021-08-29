c9bede728288ce21b5d635a342db7df315539cda
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-08-29 21:48:06 +0800

    Project CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :users do 
       devise_for :bounty_hunters, controllers: { registrations: 'v1/users/registrations', sessions: 'v1/users/sessions' }
     end
+    resources :projects
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
