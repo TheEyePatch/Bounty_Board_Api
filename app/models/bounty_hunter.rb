a5715f733b190275e92487551dd89353a474046f
Leif Bersamina <leifbersamina@gmail.com>
2021-08-30 23:07:24 +0800

    add many_to_many relationship between project, bounty, and bounty_hunter models

--- app/models/bounty_hunter.rb
+++ app/models/bounty_hunter.rb
@@ -1,4 +1,7 @@
 class BountyHunter < User
   validates :username, presence: true
   validates :password_confirmation, presence: true
+
+  has_many :bounties
+  has_many :projects, through: :bounties
 end
