26b3506c9e8eff4b269e70ed58968f0f185409cc
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-09-01 23:01:14 +0800

    automatically chnage approved to true for Admin users

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,10 @@
 class Admin < User
+  acts_as_token_authenticatable
   validates :username, presence: true
+  
+  before_validation :set_approve_to_true, on: %i[create save]
+  
+  def set_approve_to_true
+    self.approved =true
+  end
 end
