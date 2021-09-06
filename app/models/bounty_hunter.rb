4cc587368efcbeaab1af2b6874d2e47e3b38adc3
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-09-06 23:59:27 +0800

    send email notif after bounty hunter creation

--- app/models/bounty_hunter.rb
+++ app/models/bounty_hunter.rb
@@ -5,4 +5,11 @@ class BountyHunter < User
   has_many :appointments
 #   has_many :projects, through: :bounties
   has_many :bounties, through: :appointments
+  after_create :new_sign_up_mailer
+
+  def new_sign_up_mailer
+    byebug
+    AdminMailer.new_user_waiting_for_approval(self).deliver 
+    UserMailer.welcome_send(self).deliver
+  end
 end
